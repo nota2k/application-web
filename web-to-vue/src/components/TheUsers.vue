2c00ffee61e0e45997da90f4eeeba90acad58e48
Nelly <nelly.babillon@gmail.com>
2025-03-10 12:16:20 +0100

    Ajout du composant TheUsers et suppression des icônes inutilisées

---
+++ web-to-vue/src/components/TheUsers.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+</script>
+
+<template>
+      <tbody>
+        
+      </tbody>
+</template>
+
+<style scoped>
+    thead th {
+      cursor: pointer;
+    }
+  
+    thead th:hover {
+      text-decoration: underline;
+    }
+  
+    thead th svg {
+      transition: 0.2s ease-in-out all;
+    }
+  
+    thead th svg.rotate {
+      rotate: 180deg;
+    }
+  
+    .filter-wrapper {
+      display: flex;
+      justify-content: space-between;
+      margin-bottom: 1rem;
+      gap: 20px;
+    }
+  
+    .filter-wrapper>* {
+      flex: 1;
+    }
+</style>
